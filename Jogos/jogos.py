e10dade361b687cf085c4642d02c5d0e698d36a9
LuisFelipeA <felipeadriani2@gmail.com>
2022-01-27 10:54:21 -0300

    Jogos

---
+++ Jogos/jogos.py
@@ -0,0 +1,17 @@
+import forca
+import adivinhacao
+
+
+
+print("*********************************")
+print("*********Escolha um jogo!********")
+print("*********************************")
+
+print("[1] Forca [2]Advinhação")
+
+jogo = int(input("Qual jogo quer jogar?"))
+
+if(jogo == 1):
+    print("forca")
+elif(jogo == 2):
+    print("Adivinhação")
