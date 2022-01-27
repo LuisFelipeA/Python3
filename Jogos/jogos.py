81d33c462ee0e546061d7434ca8bc36cbe4399fd
LuisFelipeA <felipeadriani2@gmail.com>
2022-01-27 11:24:54 -0300

    Escolha um jogo

--- Jogos/jogos.py
+++ Jogos/jogos.py
@@ -2,18 +2,21 @@ import forca
 import adivinhacao
 
 
+def escolha_jogo():
+    print("*********************************")
+    print("*********Escolha um jogo!********")
+    print("*********************************")
 
-print("*********************************")
-print("*********Escolha um jogo!********")
-print("*********************************")
+    print("[1] Forca [2]Advinhação")
 
-print("[1] Forca [2]Advinhação")
+    jogo = int(input("Qual jogo quer jogar? "))
 
-jogo = int(input("Qual jogo quer jogar? "))
+    if(jogo == 1):
+        print("forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Adivinhação")
+        adivinhacao.jogar()
 
-if(jogo == 1):
-    print("forca")
-    forca.jogar()
-elif(jogo == 2):
-    print("Adivinhação")
-    adivinhacao.jogar()
+if(__name__ == "__main__"):
+    escolha_jogo()
