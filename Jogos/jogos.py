9b2e8f9960b0be7c231f47dfb89236d10b2fc0cf
LuisFelipeA <felipeadriani2@gmail.com>
2022-01-27 10:57:39 -0300

    Escolha um jogo

--- Jogos/jogos.py
+++ Jogos/jogos.py
@@ -9,9 +9,11 @@ print("*********************************")
 
 print("[1] Forca [2]Advinhação")
 
-jogo = int(input("Qual jogo quer jogar?"))
+jogo = int(input("Qual jogo quer jogar? "))
 
 if(jogo == 1):
     print("forca")
+    forca.jogar()
 elif(jogo == 2):
     print("Adivinhação")
+    adivinhacao.jogar()
