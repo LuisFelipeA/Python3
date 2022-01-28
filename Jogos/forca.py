bd4446a50845dfde791ad45a8afbca26be15549d
LuisFelipeA <felipeadriani2@gmail.com>
2022-01-28 14:33:32 -0300

    Jogo da forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,6 +3,30 @@ def jogar():
     print("***Bem vindo ao jogo de forca!***")
     print("*********************************")
 
+    palavra_secreta = "felipe"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+        
+        chute = input("Digite uma letra: ").strip()
+
+        posicao = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[posicao] = letra.upper()
+            posicao = posicao + 1
+        
+        print(letras_acertadas)
+        
+        print("Jogando...")
+
+
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
