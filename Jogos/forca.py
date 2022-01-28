3ec7ed64e7913e697e1379192ff160d692fc0d04
LuisFelipeA <felipeadriani2@gmail.com>
2022-01-28 15:50:24 -0300

    Acerte a palavra da forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,29 +3,42 @@ def jogar():
     print("***Bem vindo ao jogo de forca!***")
     print("*********************************")
 
-    palavra_secreta = "felipe"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
         
-        chute = input("Digite uma letra: ").strip()
+        chute = input("Digite uma letra: ").strip().upper()
+        print(" ")
+   
 
-        posicao = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra.upper()
-            posicao = posicao + 1
+        if (chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[posicao] = letra.upper()
+                posicao = posicao + 1
         
+        else:
+            erros += 1
+
         print(letras_acertadas)
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         
-        print("Jogando...")
 
-
+    if(acertou):
+        print("Voce ganhou!!!")
+        print(" ")
+    else:
+        print("Voce perdeu!!!")
+        print(" ")
 
     print("Fim do jogo")
 
